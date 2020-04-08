d0d70fcee07e9f0dd71ebb88e1f1ea981eeb68cb
Don <mcallisterda@gmail.com>
2020-04-08 14:43:51 -0400

    part 1

--- SwiftUILayout/ContentView.swift
+++ SwiftUILayout/ContentView.swift
@@ -10,9 +10,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            EventHeader()
+            Spacer()
+        }.padding()
     }
 }
+
+struct EventHeader: View {
+    var body: some View {
+        HStack(spacing: 15) {
+            CalendarView()
+            VStack(alignment: .leading) {
+                Text("Event title").font(.title)
+                Text("Location")
+            }
+            Spacer()
+        }
+    }
+}
+
+struct CalendarView: View {
+    var eventIsVerified = true
+    
+    var body: some View {
+        Image(systemName: "calendar")
+            .resizable()  //won't resize to parent frame without
+            .padding()  // this location matters since encapsulating all prior modifiers
+            .frame(width: 50, height: 50)
+            .background(Color.red)
+            .cornerRadius(10)
+            .foregroundColor(.white)
+            .addVerifiedBadge(eventIsVerified) //ZStack extension
+    }
+}
+
+// Extensions
+
+extension View {
+    func addVerifiedBadge(_ isVerified: Bool) -> some View {
+        ZStack(alignment: .topTrailing) {
+            self
+            
+            if isVerified {
+                Image(systemName: "checkmark.circle.fill")
+                .offset(x: 3, y: -3)
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
