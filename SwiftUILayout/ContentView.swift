56ef61f9cb10e7c784a04556b20baae09f32941d
Don <mcallisterda@gmail.com>
2020-04-08 15:28:53 -0400

    Handling dynamic content and layout dependency implementation for HeightSyncedRow for Event Info Badges along bottom

--- SwiftUILayout/ContentView.swift
+++ SwiftUILayout/ContentView.swift
@@ -13,7 +13,42 @@ struct ContentView: View {
         VStack {
             EventHeader()
             Spacer()
+            EventInfoList()
         }.padding()
+    }
+}
+
+struct EventInfoList: View {
+    var body: some View {
+        HeightSyncedRow(background: Color.secondary.cornerRadius(10)) {
+            EventInfoBadge(iconName: "video.circle.fill", text: "Video call available")
+            EventInfoBadge(iconName: "doc.text.fill", text: "Files are attached")
+            EventInfoBadge(iconName: "person.crop.circle.badge.plus", text: "Invites enabled, 5 people maximum")
+        }
+    }
+}
+
+// row of "info badges" at bottom
+struct EventInfoBadge: View {
+    var iconName: String
+    var text: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: iconName)
+                .resizable()
+                //fit it content into its bounds when resized:
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+            Text(text)
+                // scale as much as possible along parent (HStack) before splitting up into multiple lines:
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.center)
+        }
+        .padding(.vertical, 10)
+        .padding(.horizontal, 5)
+    //.background(Color.secondary) -> moved to HeightSyncedRow
+        .cornerRadius(10)
     }
 }
 
@@ -45,9 +80,60 @@ struct CalendarView: View {
     }
 }
 
-// Extensions
+// Extensions and Helpers To Reuse:
+
+struct HeightSyncedRow<Background: View, Content: View>: View {
+    private let background: Background
+    private let content: Content
+    @State private var childHeight: CGFloat?
+    
+    // ViewBuilder attribute to allow replace HStack with
+    // this HeightSyncedRow view
+    init(background: Background,
+         @ViewBuilder content: () -> Content) {
+        self.background = background
+        self.content = content()
+    }
+    
+    var body: some View {
+        HStack {
+            content.syncingHeightIfLarger(than: $childHeight)
+                .frame(height: childHeight)
+                .background(background)
+        }
+    }
+}
+
+// Compute the childHeight value that HeightSyncedRow
+// will assign to each of its children.
+// Each child to report current height updwards
+private struct HeightPreferenceKey: PreferenceKey {
+    // preference's default value
+    static let defaultValue: CGFloat = 0
+    // reduce two values (previous and next one) into one:
+    static func reduce(value: inout CGFloat,
+                       nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
 
 extension View {
+    func syncingHeightIfLarger(than height: Binding<CGFloat?>) -> some View {
+        background(GeometryReader { proxy in
+            // we have to attach our preference assignment to
+            // some form of view, so we just us a clear color
+            // here to make that view completely transparent:
+            Color.clear.preference(
+                key: HeightPreferenceKey.self,
+                value: proxy.size.height
+            )
+        })
+            .onPreferenceChange(HeightPreferenceKey.self) {
+                height.wrappedValue = max(height.wrappedValue ?? 0, $0)
+        }
+    }
+    
+    
     func addVerifiedBadge(_ isVerified: Bool) -> some View {
         ZStack(alignment: .topTrailing) {
             self
